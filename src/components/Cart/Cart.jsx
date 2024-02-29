08e938bd09b5411050bcc86d9ad5341415aeac41
Anujeet Swain <anujeet98@gmail.com>
2024-02-29 20:05:51 +0530

    implemented modal actions- open/close

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,17 +3,17 @@ import classes from './Cart.module.css'
 import Modal from '../Modal/Modal';
 
 
-const Cart = () => {    
+const Cart = (props) => {    
     const cartItems = 23;
     return (
-        <Modal>
+        <Modal onHideCart={props.onHideCart}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>Rs. 100</span>
             </div>
             <div className={classes.btnGrp}>
-                <button className={classes['button--cancel']}>Close</button>
+                <button className={classes['button--cancel']} onClick={props.onHideCart}>Close</button>
                 <button className={classes.button}>Order</button>
             </div>
         </Modal>
--- src/components/Layout/CartBtn.jsx
+++ src/components/Layout/CartBtn.jsx
@@ -1,8 +1,8 @@
 import './CartBtn.css'
 
-const CartBtn = () => {
+const CartBtn = (props) => {
     return (
-        <button className="cart-btn">    
+        <button className="cart-btn" onClick={props.onClick}>    
             <i className="ri-shopping-cart-line cart-icon"></i>
             <div className='cart-text'> Your Cart </div>
             <div className='cart-quantity'>0</div>
